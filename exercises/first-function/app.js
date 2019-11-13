6a6113a7dc0066c574deff0433aafb54e41809ca
Evan Sutton <evan.c.sutton1@gmail.com>
2019-11-13 15:38:34 -0700

    myFirstFunction

---
+++ exercises/first-function/app.js
@@ -0,0 +1,16 @@
+var num1 = 15
+var num2 = 25
+var num3 = 17
+var num4 = 63
+
+function sum(a, b){
+    console.log(a + b)
+}
+
+sum(num1, num2);
+
+function subtract(a, b){
+    console.log(a-b)
+}
+
+subtract(num3, num4)
