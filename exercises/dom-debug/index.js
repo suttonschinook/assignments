c458b09794eb89430202b4a2f35dfbd0430ac177
Evan Sutton <evan.c.sutton1@gmail.com>
2019-11-19 13:33:04 -0700

    DOM-DEBUG

---
+++ exercises/dom-debug/index.js
@@ -0,0 +1,30 @@
+const colors = ["red", "blue", "green"]
+
+document.getElementById("add").addEventListener("click", function (e){
+    const subItem = createSubItem(e)
+    subItem.textContent = e.target.value
+    document.getElementById("list").appendChild(subItem);
+})
+function createDropDown(){
+    const dropDown = document.createElement("select")
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
+        option.innerHTML = colors[i]
+        option.value = colors[i]
+        dropDown.append(option)
+    }
+    dropDown.addEventListener("onChange", function(e){
+        e.target.parent.backgroundColor = e.target.value
+    })
+    return dropDown
+}
+
+function createSubItem(e){
+    const subItem = document.createElement("div")
+    var subItemValue = document.getElementById("input")
+    subItem.textContent = subItemValue
+    const dropDown = createDropDown()
+    subItem.appendChild(dropDown)
+    subItem.setAttribute("class", "subItem")
+    return subItem
+}
