99df0e2542fa73380d15d6331e7b33a0e58da4e4
Evan Sutton <evan.c.sutton1@gmail.com>
2019-11-21 16:18:45 -0700

    marioPestControlFinal

---
+++ projects/marioPestControl/app.js
@@ -0,0 +1,18 @@
+function sum (num1, num2, num3){
+    var result = num1 * 5 + num2 * 7 + num3 * 11
+    return result
+}
+
+var submitBtn = document.getElementById("submitBtn")
+var moneyOwed = document.getElementById("moneyOwed")
+
+submitBtn.addEventListener("click", function(e){
+    e.preventDefault()
+    var num1 = document.getElementById("goombaNum").value
+    var num2 = document.getElementById("bobOmbNum").value
+    var num3 = document.getElementById("cheepCheepNum").value
+    var addResult = document.createElement("div")
+    moneyOwed.appendChild(addResult)
+    addResult.classList.add("results")
+    addResult.textContent = " The lazy-ass Princess owes you " + (sum(Number(num1), Number(num2), Number(num3)) + " coins of blood money for your murderous work")
+})
