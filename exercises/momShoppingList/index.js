e2300507ae4ce0e43f9633878468ce03dc1907d0
Evan Sutton <evan.c.sutton1@gmail.com>
2019-11-25 16:44:43 -0700

    momShoppingList

--- exercises/momShoppingList/index.js
+++ exercises/momShoppingList/index.js
@@ -6,14 +6,31 @@ submitBtn.addEventListener("click", function (e){
     const newItem = document.getElementById("newItem")
     const list = document.getElementById("list")
     const newListItem = document.createElement("li")
+    const newItems = document.createElement("div")
     const deleteButton = document.createElement("button")
     const editButton = document.createElement("button")
     list.appendChild(newListItem)
-    newListItem.className = "newItem"
-    newListItem.textContent = newItem.value
+    newListItem.appendChild(newItems)
+    newItems.textContent = newItem.value
     newListItem.appendChild(editButton)
     editButton.textContent = "edit"
     newListItem.appendChild(deleteButton)
     deleteButton.textContent = "X"
-    
-})
+    const xButton = document.getElementsByTagName("button")
+    for (i = 0; i < xButton.length; i++){
+    xButton[i].addEventListener("click", function(e){
+        if (e.target.textContent == "X"){
+        e.target.parentNode.remove()
+        }
+    })
+}
+
+    })
+const xButton = document.getElementsByTagName("button")    
+for (i = 0; i < xButton.length; i++){
+    xButton[i].addEventListener("click", function(e){
+        if (e.target.textContent == "X"){
+        e.target.parentNode.remove()
+        }
+    })
+}
