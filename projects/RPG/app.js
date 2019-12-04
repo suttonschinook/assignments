96c5e54e67199a8b84c222922159a34fe263184d
Evan Sutton <evan.c.sutton1@gmail.com>
2019-12-04 15:18:17 -0700

    RPG Final

--- projects/RPG/app.js
+++ projects/RPG/app.js
@@ -5,9 +5,12 @@ const playerName = readline.question("\n\nWhat is your name, Fucker? : ")
 console.log("\n\n\tThat's a horrible name, I assume your parents hate you..... I guess I'll refer to you as " + playerName +", enjoy your time in hell!")
 const choices = ["Walk", "Check HP", "Check Inventory"]
 const fightChoices = ["Attack", "Run", "Check HP", "Check Inventory"]
+items = [" Some Magic Beans", " A Magic Flute", " An ordinary rock", " 1 Gold piece", " A moldy sandwich", " An O.G. 80's TRAPPER KEEPER!"]
+const randomItems = items[Math.floor(Math.random()*items.length)];
+
 let inFight = false
 let playerObject = {
-    playerHealth: 100,
+    playerHealth: 50,
     playerInventory: []
 }
 let youDied = false
@@ -17,6 +20,7 @@ let enemy = {}
 function generateRandomChance(chance){
         return Math.floor(Math.random() * chance)
     }
+
 function walk(){
         const randomNum = generateRandomChance(4)
         if(randomNum === 0){
@@ -30,8 +34,9 @@ function walk(){
 function playerDamage(){
     const damageTaken = generateRandomChance(3)
     if (damageTaken === 0){
-        let playerHealth = playerHealth - 25
-        console.log("You take 25 damage.")
+        var damageDone = Math.floor(Math.random()*15)
+        playerObject.playerHealth = playerObject.playerHealth - damageDone;
+        console.log("You take " + damageDone + " damage.")
     }else{
         console.log("The enemy misses you")
     }
@@ -51,26 +56,30 @@ function generateEnemy(){
     const enemyNum = generateRandomChance(3);
         if (enemyNum === 0){
             enemy = { type: "Hell Spawn", health: 100}
-        }else{
-            enemy = {type: "Minion", health: 100}
+        }else if (enemyNum === 1){
+            enemy = {type: "Minion", health: 50}
+        }else if (enemyNum === 2){
+            enemy = {type: "Minion's Apprentice", health: 25}
         }
 }
 function fight(){
     const fightDamage = generateRandomChance(3);
     if (fightDamage === 0){
-        console.log("You take 25 damage")
-        playerObject.playerHealth = playerObject.playerHealth -25
+        playerDamage()
         if (playerObject.playerHealth <= 0){
-            console.log("Game Over")
+            console.log("It seems you were not strong enough, Game Over")
             process.exit()
         }
     }
     else{
-        console.log("The enemy takes 25 damage")
-        enemy.health = enemy.health -25
+        var damageDone = Math.floor(Math.random()*10)
+        console.log("The enemy takes " + damageDone + " damage")
+        enemy.health = enemy.health - damageDone
         if (enemy.health <= 0){
-            console.log("You killed the " + enemy)
+            playerObject.playerInventory = playerObject.playerInventory + randomItems
+            console.log("You killed the " + enemy.type + " and found " + randomItems)
             inFight = false
+   
         }
     }
 }
@@ -86,21 +95,19 @@ while(!youDied && !inFight){
         console.log("\n\n\t\t\tYour current inventory is " + playerObject.playerInventory)
     }else if(playerSelection === -1){
         process.exit()
-    }
-}
-
-while(!youDied && inFight){
-    console.log(enemy)
-    const playerSelection = readline.keyInSelect(fightChoices, "What would you like to do?   ")
-    if (playerSelection === 0){
-        fight()
-    }else if (playerSelection === 1){
-        run()
-    }else if(playerSelection === 2){
-        console.log("Your current status is "+ playerObject.playerHealth)
-    }else if(playerSelection === 3){
-        console.log("Your current inventory is " + playerObject.playerInventory)
-    }else if(playerSelection === -1){
-        process.exit()
+    }while(!youDied && inFight){
+        console.log(enemy)
+        const playerSelection = readline.keyInSelect(fightChoices, "What would you like to do?   ")
+        if (playerSelection === 0){
+            fight()
+        }else if (playerSelection === 1){
+            run()
+        }else if(playerSelection === 2){
+            console.log("Your current status is "+ playerObject.playerHealth)
+        }else if(playerSelection === 3){
+            console.log("Your current inventory is " + playerObject.playerInventory)
+        }else if(playerSelection === -1){
+            process.exit()
+        }
     }
 }
