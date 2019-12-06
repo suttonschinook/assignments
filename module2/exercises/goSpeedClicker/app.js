702f5734250ee12bc188807ca377fc7fe82658bd
Evan Sutton <evan.c.sutton1@gmail.com>
2019-12-06 14:28:44 -0700

    goSpeedClicker

---
+++ module2/exercises/goSpeedClicker/app.js
@@ -0,0 +1,4 @@
+const form = document.form
+
+form.addEventListener("submit", (e) => e.preventDefault
+)
--- module2/exercises/travelForm/app.js
+++ module2/exercises/travelForm/app.js
@@ -1,15 +1,13 @@
-var submitBtn = document.form
+const form = document.form
+const restrictions = []
 
-
-
-submitBtn.addEventListener("submit", function(e){
+form.addEventListener("submit", function(e){
     e.preventDefault()
-    var destination = document.getElementById("destination").value
-    var firstName = document.getElementById("firstName").value
-    var lastName = document.getElementById("lastName").value
-    var age = document.getElementById("age").value
-    var restriction1 = document.form.restriction1.value
-    var restriction2 = document.form.restriction2.value
-    var restriction3 = document.form.restriction3.value
-    window.alert("Hello! Thank you for chosing Scandanaia Air. \n First Name:"+ firstName "\n Last Name:" + lastName "\n Age:" + age, "\n Destination:" + destination) 
+    const destination = form.destination.value
+    const firstName = form.firstName.value
+    const lastName = form.lastName.value
+    const age = form.age.value
+    const restriction = form.restriction.value
+    console.log(destination, firstName, lastName)
+    window.alert("Hello! Thank you for chosing Scandanaia Air. \n First Name: "+ firstName + "\n Last Name: " + lastName + "\n Age: " + age, "\n Destination: " + destination + "\n Restrictions: " + restriction) 
 })
