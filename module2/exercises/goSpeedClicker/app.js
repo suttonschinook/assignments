f4c284799cea837c1ab066406ab8e3ec380b3a0e
Evan Sutton <evan.c.sutton1@gmail.com>
2019-12-06 15:38:28 -0700

    goSpeedClicker

--- module2/exercises/goSpeedClicker/app.js
+++ module2/exercises/goSpeedClicker/app.js
@@ -1,4 +1,17 @@
 const form = document.form
+const reset = document.getElementById("reset");
+let count = localStorage.getItem("counter") || 0
+document.getElementById("counter").textContent = count
 
-form.addEventListener("submit", (e) => e.preventDefault
-)
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+    count++;
+    document.getElementById("counter").textContent = count
+    localStorage.setItem("counter", count)
+    })
+
+reset.addEventListener("click", function(e){
+    e.preventDefault()
+    count = 0;
+    document.getElementById("counter").textContent = count
+})
