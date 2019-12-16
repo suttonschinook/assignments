4bcbd1776ae33090d51dc28178f54edb8f6b55cb
Evan Sutton <evan.c.sutton1@gmail.com>
2019-12-16 09:53:13 -0700

    thePrincessIsInAnotherCastle

--- module2/exercises/thePrincessIsInAnotherCastle/app.js
+++ module2/exercises/thePrincessIsInAnotherCastle/app.js
@@ -1,41 +1,58 @@
+
 class Player {
     constructor (){
         this.name = "Mario"
         this.totalCoins = 0
         this.status = "small"
         this.hasStar = false
-        this.gameActive = true
     }
     setName(namePicked){
          this.name = namePicked
     }
-    gotHit(){
-        if(this.stauts === "small"){
-            this.status === "DEAD"
-            this.gameActive === false
+    gotHit() {
+        if(this.status === "small"){
+            this.status = "DEAD"
         }else if(this.status === "BIG"){
-            this.status === "small"
+            this.status = "small"
         }else{
-            this.status === "BIG"
+            this.status = "BIG"
         }
     }
     gotPowerUp(){
-        if(this.stauts === "small"){
-            this.status === "BIG"
+        if(this.status === "small"){
+            this.status = "BIG"
         }else if(this.status === "BIG"){
-            this.status === "Powered Up"
-            this.hasStar === true
+            this.status = "Powered Up"
+            this.hasStar = true
         }
     }
     addCoin(){
-        this.totalCoins = this.totalCoins++
+        this.totalCoins += 1
     }
     print(){
-        console.log({
-            "Name" : Player.name,
-            "Status" : Player.status, 
-            "Coins" : Player.totalCoins,
-            "Has Star" : Player.hasStar,
-        })
+        console.log(`\n\nName: ${this.name} \nStatus: ${this.status} \nCoins: ${this.totalCoins} \nHas Star: ${this.hasStar}`)
+        return 'Hi'
+    }
+}
+
+let gameActive = true
+let character = new Player()
+
+const generateRandomChance = () =>{
+    return Math.floor(Math.random() * 3)
+}
+const intervalId = setInterval(goGame, 1000)
+function goGame(){
+    if (character.status === "DEAD"){
+        clearInterval(intervalId)
+    }
+    character.print()
+    let chance = generateRandomChance()
+    if(chance === 0){
+        character.gotHit();
+    }else if(chance === 1){
+        character.gotPowerUp()
+    }else{
+        character.addCoin()
     }
 }
