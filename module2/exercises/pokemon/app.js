0cc4ef27a0406146aa744b4a6cb57f28aee6ef52
Evan Sutton <evan.c.sutton1@gmail.com>
2019-12-17 16:11:48 -0700

    pokemon

---
+++ module2/exercises/pokemon/app.js
@@ -0,0 +1,14 @@
+const xhr = new XMLHttpRequest()
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata)
+        console.log(data)
+    }else if(xhr.readyState === 4 && xhr.status !== 200){
+        console.log(xhr.responseText)
+    }
+}
