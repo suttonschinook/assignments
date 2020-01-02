ae4eaeef82ae0b2a91de85f21dd3b7ed4f86bfef
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-02 16:27:37 -0700

    axiosToDo

--- module2/projects/axiosToDo/app.js
+++ module2/projects/axiosToDo/app.js
@@ -2,15 +2,33 @@ const listTodos = () => {
     axios.get("https://api.vschool.io/evansutton/todo")
     .then (response => {
         for(i =0; i < response.data.length; i++){
-            const unorderdList = document.createElement("ul")
-            unorderdList.setAttribute("id", "ul")
-            const listItem = document.createElement("li")
+            const unorderdList = document.createElement("span")
+            unorderdList.setAttribute("id", response.data[i]._id)
+
+            const checkbox = document.createElement('input')
+            checkbox.setAttribute("type" ,"checkbox")
+
+            const listItem = document.createElement("h1")
             listItem.textContent = response.data[i].title
+
             const itemImage = document.createElement("img")
             itemImage.setAttribute("id", "images")
-            itemImage.imgUrl = response.data[i].imgUrl
-            console.log(itemImage.imgUrl)
-            document.getElementById("display").appendChild(listItem, itemImage)
+            itemImage.src = response.data[i].imgUrl
+            itemImage.style.width="200px"
+
+            const completeBtn = document.createElement("BUTTON")
+            completeBtn.setAttribute("id", "complete")
+            completeBtn.textContent = "Complete"
+
+            const deleteBtn = document.createElement("BUTTON")
+            deleteBtn.setAttribute("id", "delete")
+            deleteBtn.textContent = "DELETE"
+
+            document.getElementById("display").appendChild(listItem)
+            document.getElementById("display").appendChild(itemImage)
+            document.getElementById("display").appendChild(checkbox)
+            document.getElementById("display").appendChild(deleteBtn)
+            console.log(unorderdList)
         }if (response.data.completed = false){
             listItem.style.textDecoration = "line-through"
         }
@@ -18,6 +36,13 @@ const listTodos = () => {
     
     .catch (error => console.log(error))
 }
+
+// completeBtn.addEventListener("click", function(e){
+//     e.preventDefault()
+//     if (display.hasChildNodes.checkbox.checked = true){
+        
+//     }
+// })
 
 const form = document.form
 
