a5b5423ced97fa404a2be734a63ec380c2d61313
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-13 09:16:28 -0700

    vacationSpots

---
+++ module3/exercises/vacation-spots/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import vacationSpots from "./vacationSpots"
+
+function App() {
+    return(
+        <div>
+            <VacationSpot />
+            <VacationSpot />
+            <VacationSpot />
+            <VacationSpot />
+            <VacationSpot />
+        </div>
+    )
+}
+
+export default App
---
+++ module3/exercises/vacation-spots/VacationSpot.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function VacationSpot(props){
+    return(
+        <div>
+            <h2>{props.vactionSpots.place}</h2>
+        </div>
+    )
+}
+export default VacationSpot
