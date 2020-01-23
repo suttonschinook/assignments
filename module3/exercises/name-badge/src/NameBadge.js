3a190e3b0bd73fd9c25413ea97ca92a509151e3b
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-23 14:23:31 -0700

    name-badge

--- module3/exercises/name-badge/src/NameBadge.js
+++ module3/exercises/name-badge/src/NameBadge.js
@@ -14,17 +14,56 @@ class NameBadge extends React.Component{
         }
     }
     render(){
+        const badgeStyle = {
+            display: "flexbox",
+            height: "300px",
+            width: "680px",
+            border: "2px solid black",
+            borderRadius: "1%",
+            marginLeft: "auto",
+            marginRight: "auto",
+
+        }
+        const inputStyle = {
+            border: "2px solid black",
+            borderRadius: "1%",
+            width: "250px",
+            marginLeft: "55px",
+            marginTop:"20px"
+        }
+        const aboutStyle ={
+            marginTop: "20px",
+            width: "561px",
+            height: "100px",
+            marginLeft: "55px",
+            border: "2px solid black",
+            borderRadius: "1%"
+        }
+        const formStyle = {
+            display: "flexbox",
+            alignItems: "center",
+            justifyContent: "space-between"
+        }
+        const buttonStyle = {
+            marginLeft: "43%",
+            marginTop: "10px",
+            width: "100px",
+            border: "2px solid black",
+            borderRadius: "1%"
+           
+        }
         return(
-            <div>
-                <form>
-                    <input placeholder="First Name"></input>
-                    <input placeholder="Last Name"></input>
-                    <input placeholder="Email"></input>
-                    <input placeholder="Place of Birth"></input>
-                    <input placeholder="Phone"></input>
-                    <input placeholder="Favorite Food"></input>
-                    <input placeholder="Tell us about yourself "></input>
-                    <button>Submit</button>
+            <div style={badgeStyle}>
+                <form style={formStyle}>
+                    <input style = {inputStyle} placeholder="First Name"></input>
+                    <input style = {inputStyle} placeholder="Last Name"></input>
+                    <input style = {inputStyle} placeholder="Email"></input>
+                    <input style = {inputStyle} placeholder="Place of Birth"></input>
+                    <input style = {inputStyle} placeholder="Phone"></input>
+                    <input style = {inputStyle} placeholder="Favorite Food"></input>
+                    <textarea style = {aboutStyle} placeholder="Tell us about yourself " ></textarea>
+                    <br/>
+                    <button style = {buttonStyle} >Submit</button>
                 </form>
             </div>
         )
--- module3/exercises/name-badge/src/index.js
+++ module3/exercises/name-badge/src/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom"
 import NameBadge from "./NameBadge"
 
 ReactDOM.render(<NameBadge />, document.getElementById("root"))
