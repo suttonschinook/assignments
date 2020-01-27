26b86076fad09d5dbdb2c04423bf750056ce47be
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-27 11:29:11 -0700

    badge-card

---
+++ module3/exercises/name-badge/src/BadgeCard.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function BadgeCard(props){
+    console.log(props)
+    return(
+        <div style = {props.badgeStyle.badgeStyle}>
+            <div>{props.firstName}</div>
+            <div>{props.lastName}</div>
+            <div>{props.favfood}</div>
+            <div>{props.phoneNumber}</div>
+            <div>{props.placeOfBirth}</div>
+            <div>{props.emailAddress}</div>
+            <div>{props.aboutMe}</div>
+        </div>
+    )
+}
+
+export default BadgeCard
--- module3/exercises/name-badge/src/NameBadge.js
+++ module3/exercises/name-badge/src/NameBadge.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./styles.css"
+import BadgeCard from "./BadgeCard"
 
 class NameBadge extends React.Component{
     constructor(){
@@ -10,9 +11,42 @@ class NameBadge extends React.Component{
             phoneNumber: "",
             placeOfBirth:"",
             emailAddress: "",
-            favFood: ""
+            favFood: "",
+            aboutMe: "",
+            badges: [],
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+    handleChange(e){
+        this.setState({[e.target.name] : e.target.value})
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        this.setState(prevState => {
+            const {firstName, lastName, phoneNumber, placeOfBirth, emailAddress, favFood, aboutMe} = prevState
+            const newBadge= {
+                firstName,
+                lastName,
+                phoneNumber,
+                placeOfBirth,
+                emailAddress,
+                favFood,
+                aboutMe
+            }
+            return{
+                badges: [...prevState.badges, newBadge],
+                firstName: "",
+                lastName: "",
+                phoneNumber: "",
+                placeOfBirth:"",
+                emailAddress: "",
+                favFood: "",
+                aboutMe: "",
+            }
+        })
+    }
+
     render(){
         const badgeStyle = {
             display: "flexbox",
@@ -52,19 +86,26 @@ class NameBadge extends React.Component{
             borderRadius: "1%"
            
         }
+
+        const mappedBadges = this.state.badges.map((badge, index) => {
+            return(
+                <BadgeCard  key = {index} {...badge} badgeStyle={buttonStyle, formStyle, aboutStyle, inputStyle, badgeStyle}/>
+            )
+        })
         return(
             <div style={badgeStyle}>
-                <form style={formStyle}>
-                    <input style = {inputStyle} placeholder="First Name" minLength = "3" ></input>
-                    <input style = {inputStyle} placeholder="Last Name" minLength = "3"></input>
-                    <input style = {inputStyle} placeholder="Email" minLength = "3"></input>
-                    <input style = {inputStyle} placeholder="Place of Birth" minLength = "3"></input>
-                    <input type = "number" style = {inputStyle} placeholder="Phone" minLength = "9"></input>
-                    <input style = {inputStyle} placeholder="Favorite Food" minLength = "3"></input>
-                    <textarea style = {aboutStyle} placeholder="Tell us about yourself "></textarea>
+                <form onSubmit = {this.handleSubmit} style={formStyle}>
+                    <input value = {this.state.firstName} onChange = {this.handleChange} name = "firstName" style = {inputStyle} placeholder="First Name" minLength = "3" ></input>
+                    <input value = {this.state.lastName} onChange = {this.handleChange}  name = "lastName" style = {inputStyle} placeholder="Last Name" minLength = "3"></input>
+                    <input value = {this.state.emailAddress} onChange = {this.handleChange}  name = "emailAddress" style = {inputStyle} placeholder="Email" minLength = "3"></input>
+                    <input value = {this.state.placeOfBirth} onChange = {this.handleChange}  name = "placeOfBirth" style = {inputStyle} placeholder="Place of Birth" minLength = "3"></input>
+                    <input value = {this.state.phoneNumber} onChange = {this.handleChange} name = "phoneNumber" type = "number" style = {inputStyle} placeholder="Phone" minLength = "9"></input>
+                    <input value = {this.state.favFood} onChange = {this.handleChange} name = "favFood" style = {inputStyle} placeholder="Favorite Food" minLength = "3"></input>
+                    <textarea value = {this.state.aboutMe} onChange = {this.handleChange} name = "aboutMe" style = {aboutStyle} placeholder="Tell us about yourself "></textarea>
                     <br/>
                     <button style = {buttonStyle} >Submit</button>
                 </form>
+                {mappedBadges}
             </div>
         )
     }
