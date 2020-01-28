83cbe9c4e135bff7c00bee86d3c9c716e42350e5
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-28 14:03:55 -0700

    render-practice

---
+++ module3/exercises/render-practice/src/App.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            loggedIn: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(){
+        this.setState(prevState => {
+            return {
+                loggedIn: !prevState.loggedIn
+            }
+        })
+    }
+
+    render(){
+        console.log(this.state.loggedIn)
+        let buttonText = this.state.loggedIn ? "Log Out" : "Log In"
+        return (
+            <div>
+                <button onClick = {this.handleClick}>{buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default App
