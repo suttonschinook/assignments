f874812e625c1afa9ec63d58c8db6921227d8e0a
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-29 10:57:11 -0700

    hit-list

--- module3/exercises/hit-list/src/Header.js
+++ module3/exercises/hit-list/src/Header.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 function Header (){
+    const nameStyle ={
+        display: "flex",
+        justifyContent: "center",
+        color: "red",
+        fontSize: "50px"
+    }
     return(
-        <div>
-            <span>Don Vito Corleone's Hit list</span>
+        <div style = {nameStyle}>
+            <p>Don Vito Corleone's Hit list</p>
         </div>
     )
 }
