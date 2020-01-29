572c0b0b34cfaa609c78adab0410763d6ea4b3e7
Evan Sutton <evan.c.sutton1@gmail.com>
2020-01-29 13:30:59 -0700

    random-color-component

---
+++ module3/exercises/random-color-component/src/App.js
@@ -0,0 +1,46 @@
+import React from "react"
+import "./styles.css"
+const axios = require ("axios")
+
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            randomColor: ""
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(){
+        axios.get("http://www.colr.org/json/color/random")
+        .then(response => response.data)
+        .then(data => {
+            this.setState({
+                randomColor: data.colors[0].hex
+            })
+        })
+    }
+
+    componentDidMount(){
+        axios.get("http://www.colr.org/json/color/random")
+        .then(response => response.data)
+        .then(data => {
+            this.setState({
+                randomColor: data.colors[0].hex
+            })
+            console.log(data)
+        })
+    }
+
+    render(){
+        const backgroundColor ={
+            backgroundColor: `#${this.state.randomColor}`
+        }
+        return(
+            <div style = {backgroundColor}>
+                <h1>Random Color Time</h1>
+                <button onClick={this.handleClick}>Change the Color</button>
+            </div>
+        )
+    }
+}
+export default App
