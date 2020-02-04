c3e5620332f4788341d29f6980cd0f22c802ee98
Evan Sutton <evan.c.sutton1@gmail.com>
2020-02-04 10:40:42 -0700

    meme-generator

--- module3/projects/meme-generator/src/MemeGenerator.js
+++ module3/projects/meme-generator/src/MemeGenerator.js
@@ -2,33 +2,29 @@ import React from "react"
 import "./styles.css"
 
 class MemeGenerator extends React.Component{
-    constructor(){
-        super();
-        this.state = {
+    state = {
             topText: "",
             bottomText: "",
             randomImage: "http://i.imgflip.com/1bij.jpg",
             allMemeImages: [],
             starWars:[]
         }
-        this.handleChange = this.handleChange.bind(this)
-        this.generateNewMeme = this.generateNewMeme.bind(this)
-    }
-    handleChange(e){
+
+    handleChange = (e) => {
         const {name, value} = e.target
         this.setState({
             [name]: value
         })
         
     }
-    generateNewMeme(e){
+    generateNewMeme = (e) => {
         e.preventDefault()
         const randomNumber = Math.floor(Math.random() * this.state.allMemeImages.length)
         const randomMemeImage = this.state.allMemeImages[randomNumber].url
         this.setState({ randomImage: randomMemeImage})
     }
 
-    componentDidMount(){
+    componentDidMount = () => {
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
             .then(response => {
@@ -40,6 +36,7 @@ class MemeGenerator extends React.Component{
         }
     
     render(){
+        const {topText, bottomText, handleChange, randomImage, generateNewMeme} = this.state
         return(
             <div>
                 <form className="meme-form">
@@ -48,23 +45,23 @@ class MemeGenerator extends React.Component{
                         className = "topText"
                         name = "topText"
                         placeholder = "Top Text"
-                        value = {this.state.topText}
-                        onChange = {this.handleChange}
+                        value = {topText}
+                        onChange = {handleChange}
                     />
                     <input
                         type = "text"
                         className = "bottomText"
                         name = "bottomText"
                         placeholder = "Bottom Text"
-                        value = {this.state.bottomText}
-                        onChange = {this.handleChange}
+                        value = {bottomText}
+                        onChange = {handleChange}
                     />
-                    <button onClick ={this.generateNewMeme}>Gen</button>
+                    <button onClick ={generateNewMeme}>Gen</button>
                 </form>
                 <div className="meme">
-                    <img className = "memeImage" src={this.state.randomImage} />
-                    <h2 className ="top">{this.state.topText}</h2>
-                    <h2 className ="bottom">{this.state.bottomText}</h2>
+                    <img className = "memeImage" src={randomImage} />
+                    <h2 className ="top">{topText}</h2>
+                    <h2 className ="bottom">{bottomText}</h2>
                 </div>
             </div>
         )
