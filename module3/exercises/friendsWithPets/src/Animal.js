50722653aa9371074b3840444ef56ba34f9864cd
Evan Sutton <evan.c.sutton1@gmail.com>
2020-02-05 13:52:40 -0700

    update

--- module3/exercises/friendsWithPets/src/Animal.js
+++ module3/exercises/friendsWithPets/src/Animal.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { Fragment } from "react"
 
 function Animal(props){
     return(
-        <div>
+        <>
             <h2 class="petName">{props.animal.name}</h2>
             <h2 class="breed">{props.animal.breed}</h2>
-        </div>
+        </>
     )
 }
 
--- module3/exercises/friendsWithPets/src/App.js
+++ module3/exercises/friendsWithPets/src/App.js
@@ -5,9 +5,9 @@ import FriendsInfo from "./FriendsInfo"
 function App (){
     const friendList = friends.map(friends => <FriendsInfo FriendsInfo={friends}/>)
     return(
-        <div>
+        <>
            {friendList}
-        </div>
+        </>
     )
 }
 
